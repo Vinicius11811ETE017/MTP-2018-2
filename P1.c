6c7c3d366f9e915532c216fb40f44c65cf059704
Vinicius11811ETE017 <42479216+Vinicius11811ETE017@users.noreply.github.com>
2018-09-11 21:10:57 -0300

    Update P1.c

--- P1.c
+++ P1.c
@@ -1,39 +1,29 @@
 #include <stdio.h>
-
-int main()
-{
-    int i = 0;
-    char nro[256];
-    int estado = 0;
-    printf("Digite o nro: \n");
-    scanf("%s",nro);
-    while(nro[i] != '0'){
-        if(estado == 0 && nro[i] == '0'){
-        estado = 0;
-        }
-        else{
-            estado = 1;
-        }
-        if(estado == 1 && nro[i] == '0'){
-        estado = 2;
-        }
-        else{
-            estado = 0;
-        }
-        if(estado == 2 && nro[i] == '0'){
-        estado = 1;
-        }
-        else{
-            estado = 2;
-        }
-        
-            i++;
+ 
+int main(){
+int estado = 0;
+int i = 0;
+char nro[256];
+printf("Digite o numero em binario: \n");
+scanf("%s", nro);
+while(nro[i] != '\0'){
+if(nro[i] == '0'){
+if(estado == 0);
+else if(estado == 1) estado = 2;
+else if(estado == 2) estado = 1;
+i++;
 }
-if(estado == 0){
-    printf("E multiplo de 3");
+else if(nro[i] == '1'){
+if(estado == 0) estado = 1;
+else if(estado == 1) estado = 0;
+else if(estado == 2);
+i++;
 }
-else {
-    printf("Nao e multiplo de 3");
-}
-    return 0;
+else{
+printf("O numero que foi digitado nao e binario\n");
+ return 0;
+}}
+if(estado == 0) printf("O numero %s e multiplo de 3\n",nro);
+else printf("O numero %s nao e multiplo de 3\n",nro);
+return 0;
 }
